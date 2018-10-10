6fe79c5939e4c727152eeeaf76b5e29fd3d354b9
Liang <jeremy.shi@blockchaintech.net.au>
2018-10-10 15:33:48 +1100

    add readme and more examples

--- example.php
+++ example.php
@@ -47,9 +47,22 @@ $res = $stringExchange->buy($order);
 
 $orderId = 'd0822e38-4ae2-40e1-b731-cc783ea7f060';
 // cancel an order, pass the order uuid
-$res = $stringExchange->cancel($orderId);
+// $res = $stringExchange->cancel($orderId);
 
 $orderId = '943585b8-8283-4971-96da-0da6a7271bdf';
-$res = $stringExchange->order($orderId);
+// $res = $stringExchange->order($orderId);
+
+// get open orders from all symbols with default limit 20
+$res = $stringExchange->openOrders();
+
+// get open orders from "ETH/AUD" with limit 100
+$res = $stringExchange->openOrders('ETH/AUD', 100);
+
+// get my trades from "BTC/AUD" with default limit 20
+$res = $stringExchange->myTrades();
+
+// get my trades from "ETH/AUD" with limit 100
+$res = $stringExchange->myTrades('BTC/AUD', 1);
+
 printf(json_encode($res));
 
